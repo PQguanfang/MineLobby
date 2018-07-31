e63eb4d5c834522d7d38f77c802e4639571489c0
PQguanfang <35395058+PQguanfang@users.noreply.github.com>
2018-08-01 07:43:58 +0800

    Update Loader.php

--- src/Loader.php
+++ src/Loader.php
@@ -1,1 +1,20 @@
+<?php
 
+namespace MineLobby;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+class CmdSnooper extends PluginBase {
+	
+	public function onEnable(): void {
+		@mkdir($this->getDataFolder());
+		$this->getLogger()->info("§a本插件已成功加载.§b让我们开始体验MineLobby吧. >:D");
+		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+		$this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML, array(
+	  	"Join" => "true",
+  		));
+	}
+	
